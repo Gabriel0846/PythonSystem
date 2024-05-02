510431131c21650ab3d7c963bbac584bf02f0e0a
Gabriel Lopes <carnagesagas@gmail.com>
2024-05-02 10:09:25 -0300

    ajustes

--- cliente-1/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Terminal17\Documents\GitHub\PythonSystem\cliente-1\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
