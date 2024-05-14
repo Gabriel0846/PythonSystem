4b39ec45ee2815311ae3a1c12c76bfde989f902e
Gabriel Lopes <carnagesagas@gmail.com>
2024-05-14 10:14:32 -0300

    alguns testes com listas

--- panda.py
+++ panda.py
@@ -9,3 +9,22 @@ print(series_objeto2)
 disciplinas = {'cursos' : ['Estatística', 'Economia', 'Cálculo', 'Geometria'], 'créditos' : [90, 60, 90, 40], 'requisito' : [True, False, True, False]}
 data = pd.DataFrame(disciplinas)
 print(data)
+
+nome_cidade = pd.Series(['Maringá', 'Itabira', 'Uberlândia'])
+populacao = pd.Series([ 403.063, 120.904, 699.097])
+cidade = pd.DataFrame({"Cidade": nome_cidade, "População": populacao})
+print(cidade)
+
+cidades = ['Maringá', 'Itabira', 'Uberlândia', 'Salvador', 'Fortaleza']
+populacoes = [403.063, 120.904, 699.097, 2.886698, 2.686612]
+
+populacao_cidades = dict(zip(cidades, populacoes))
+print(populacao_cidades)
+
+print(type(populacao_cidades))
+print(populacao_cidades['Uberlândia'])
+print('Maringá' in populacao_cidades)
+print('São Carlos' in populacao_cidades)
+
+populacao_cidades['Vitória'] = 365.855
+print(populacao_cidades)
