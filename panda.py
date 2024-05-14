095723c5b3c92b5d95da9124a862a2fdd0accb91
Gabriel Lopes <carnagesagas@gmail.com>
2024-05-14 09:30:03 -0300

    teste com dataframe

--- panda.py
+++ panda.py
@@ -5,3 +5,7 @@ print(series_objeto)
 
 series_objeto2 = pd.Series([4, 7, 8, -2], index=['alfa', 'beta', 'teta', 'gama'])
 print(series_objeto2)
+
+disciplinas = {'cursos' : ['Estatística', 'Economia', 'Cálculo', 'Geometria'], 'créditos' : [90, 60, 90, 40], 'requisito' : [True, False, True, False]}
+data = pd.DataFrame(disciplinas)
+print(data)
