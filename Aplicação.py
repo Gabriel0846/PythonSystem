c8f7a59fcc554526d4a9dbe48e79f11d8b44c3dd
Gabriel Lopes <carnagesagas@gmail.com>
2024-05-14 17:29:33 -0300

    criado programa com os cruds, ainda falta finalizar

---
+++ Aplicação.py
@@ -0,0 +1,19 @@
+from GUI import *
+import Backend as core
+
+app = None
+
+def view_command():
+    rows = core.view()
+    app.listClientes.delete(0, END)
+    for r in rows:
+        app.listClientes.insert(END, r)
+
+def search_command():
+    app.listClientes.delete(0, END)
+    rows = core.search(app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), app.txtCPF.get())
+    for r in rows:
+        app.listClientes.insert(END, r)
+
+def insert_command():
+    core.insert(app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), )
